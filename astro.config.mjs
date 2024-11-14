8a16de574560c4ca89a00c4979a78c27c935ff82
SergeiKrk <krjukov_s@mail.ru>
2024-11-14 15:36:06 +0400

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
 // Import the Vercel adapter
-import vercel from '@astrojs/vercel/serverless';
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-  adapter: vercel(),
+  outDir: "dist",
 });
